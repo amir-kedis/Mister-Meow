b648d758ca5765f70a06b38dcec594725712346f
amir-kedis <amir.kedis@gmail.com>
2024-04-05 22:05:11 +0200

    build(indexer): setup gradle in MeowIndexer

---
+++ src/meowindexer/app/src/main/java/meowindexer/Main.java
@@ -0,0 +1,19 @@
+package meowindexer;
+
+public class Main {
+
+  public static void main(String[] args) {
+    final String ANSI_RESET = "\u001B[0m";
+    final String ANSI_TEAL = "\u001B[36m";
+
+    System.out.println(ANSI_TEAL);
+    System.out.println("====================================");
+    System.out.println("||     Hello from Meowindexer!    ||");
+    System.out.println("====================================");
+    System.out.println("||  Meowindexer is a  cool  tool  ||");
+    System.out.println("||  to index webpages for the fa- ||");
+    System.out.println("||       ncy MeowMister SE.       ||");
+    System.out.println("====================================");
+    System.out.println(ANSI_RESET);
+  }
+}
