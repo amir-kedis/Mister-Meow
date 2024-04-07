60998964c945ee07e6379c97579e75033d45df06
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-04-07 23:00:01 +0200

    feat(DB): Add insert into Document function

--- src/meowDBManager/app/src/main/java/meowDBManager/DBManager.java
+++ src/meowDBManager/app/src/main/java/meowDBManager/DBManager.java
@@ -1,8 +1,55 @@
 package meowDBManager;
 
+import java.net.*;
+
+import org.bson.Document;
+
 import com.mongodb.client.*;
+import com.mongodb.client.model.CreateCollectionOptions;
+import com.mongodb.MongoException;
 
 public class DBManager {
-  public static MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-  MongoDatabase DB = mongoClient.getDatabase("meowDB");
+  public MongoClient mongoClient;
+  public MongoDatabase DB;
+
+  public DBManager() {
+    mongoClient = MongoClients.create("mongodb://localhost:27017");
+    DB = mongoClient.getDatabase("meowDB");
+
+    CreateCollectionOptions options = new CreateCollectionOptions().capped(false);
+    DB.createCollection("Documents", options);
+    DB.createCollection("InvertedIndex", options);
+  }
+
+  public String insertDocument(String url, String title, String host, String content) {
+    try {
+      // Check for valid URL
+      new URL(url).toURI();
+
+      MongoCollection<Document> docCollection = DB.getCollection("Documents");
+      Document document = new Document()
+          .append("URL", url)
+          .append("title", title)
+          .append("host", host)
+          .append("content", content);
+
+      String insertedId = docCollection.insertOne(document).getInsertedId().toString();
+      return insertedId;
+
+    } catch (MalformedURLException | URISyntaxException e) {
+
+      System.out.println("Error with URL: " + e.getMessage());
+      return null;
+
+    } catch (MongoException e) {
+
+      System.out.println("Error occurred while insertion: " + e.getMessage());
+      return null;
+    }
+  }
+
+  public String insertInverted(String docID, String[] token) {
+    return null;
+  }
+
 }
