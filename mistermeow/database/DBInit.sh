8360dc978584a2feb2139fc7c0618f01f20351b5
amir-kedis <amir.kedis@gmail.com>
2024-04-09 18:25:50 +0200

    hotfix: mongo scripts permision fixed

--- mistermeow/database/DBInit.sh
+++ mistermeow/database/DBInit.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-# Start mongodb
-echo "Start MongoDB..."
-sudo systemctl start mongod
-
 if [ -d "../database/dbDump" ]; then
-  # Import database from dump file
-  echo "Import Database..."
-  mongorestore --db meowDB ../database/dbDump
+	# Import database from dump file
+	printf "\033[32m||========================||\033[0m\n"
+	printf "\033[32m||  Importing Database... ||\033[0m\n"
+	printf "\033[32m||========================||\033[0m\n"
+	mongorestore --db meowDB ../database/dbDump
 else
-  echo "Couldn't Import Database !"
+	echo "Couldn't Import Database !"
 fi
