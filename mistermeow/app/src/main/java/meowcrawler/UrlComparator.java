a91c4e16b9abaeab33bbef51d477aac2c2aa4a79
Abdelruhman Samy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-04-09 09:47:35 +0200

    chore: adding comment

--- mistermeow/app/src/main/java/meowcrawler/UrlComparator.java
+++ mistermeow/app/src/main/java/meowcrawler/UrlComparator.java
@@ -1,5 +1,7 @@
 package src.main.java.meowcrawler;
 import java.util.*;
+
+//The least priority -> comes in front in the queue
 public class UrlComparator implements Comparator<Url> {
     public int compare(Url u1 , Url u2){
         if(u1.getPriority() > u2.getPriority())
