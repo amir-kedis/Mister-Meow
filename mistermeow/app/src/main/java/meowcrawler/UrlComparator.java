6797475eee15f8362b5221d1814e29db459fb6c4
Abdelruhman Samy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-04-09 11:12:09 +0200

    feat: implementing comparator for Url

---
+++ mistermeow/app/src/main/java/meowcrawler/UrlComparator.java
@@ -0,0 +1,14 @@
+package src.main.java.meowcrawler;
+import java.util.*;
+
+//The least priority -> comes in front in the queue
+public class UrlComparator implements Comparator<Url> {
+    public int compare(Url u1 , Url u2){
+        if(u1.getPriority() > u2.getPriority())
+            return 1;
+        else if(u1.getPriority() < u2.getPriority())
+            return -1;
+        else
+            return 0;
+    }
+}
