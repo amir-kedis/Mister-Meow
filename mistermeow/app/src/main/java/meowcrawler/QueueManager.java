8e741f9e4b38f0f330383528181db3f3c2bf0e98
Abdelruhman Samy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-04-09 09:40:16 +0200

    chore: cleaning QueueManager extra code

--- mistermeow/app/src/main/java/meowcrawler/QueueManager.java
+++ mistermeow/app/src/main/java/meowcrawler/QueueManager.java
@@ -8,25 +8,17 @@ import com.google.common.collect.HashBiMap;
 public class QueueManager {
 
     private static final int domainQCapacity = 100;
-    private Queue<Url> priorityQ;                  //priority queue to priotorize urls on pushing
-    //    private LinkedList<Url>[] domainQList;        //list of domain queues
-    private PriorityQueue<Queue<Url>> domainQs;
-    private BiMap<String, Queue<Url>> domainIndexMp;  //Bidirectional-map between url and domain queue in the list
-//    private Set<Integer> EmptyQueuesSet;           //Set of indicies of empty queues inoreder to manage pop
-//    private Set<Integer> EmptyQueuesSet;           //Set of indicies of empty queues inoreder to manage pop
+    private Queue<Url> priorityQ;                       //priority queue to priotorize urls on pushing
+    private PriorityQueue<Queue<Url>> domainQs;         //list of domain queues
+    private BiMap<String, Queue<Url>> domainIndexMp;    //Bidirectional-map between url and domain queue in the list
 
-    private Integer domainQListSize;               //Denotes the total number of domain queues inside the list
-//    private Integer nextPopIndex;                  //Selects which domain queue to pop urls from
-//    private Integer urlsInDomainQ;                 //Count of total urls in all domainqueues
+    private Integer domainQListSize;                    //Denotes the total number of domain queues inside the list
 
     public QueueManager() {
         priorityQ = new PriorityQueue(1, new UrlComparator());
         domainIndexMp = HashBiMap.create();
-//        domainQList = new LinkedList[domainQCapacity];
         domainQs = new PriorityQueue(domainQCapacity, new llComparator());
         domainQListSize = 0;
-//        nextPopIndex = 0;
-//        urlsInDomainQ = 0;
     }
 
     public boolean push(String newUrl) {
@@ -41,13 +33,6 @@ public class QueueManager {
     }
 
     public String pop() throws Exception {
-//        if (domainQList[nextPopIndex] == null)
-//            throw new Exception("Trying to pop from empty queue");
-//        urlsInDomainQ--;
-//        String s = domainQList[nextPopIndex].poll().getUrlString();
-//        incrementNextPopIndex();
-//        return s;
-
         Queue<Url> q = domainQs.poll();
         if (q.isEmpty())
             throw new Exception("Trying to pop from empty queue");
@@ -65,18 +50,8 @@ public class QueueManager {
     }
 
     public boolean isEmpty() {
-//        return urlsInDomainQ == 0 ? true : false;
         return domainQs.isEmpty();
     }
-
-//    private void incrementNextPopIndex() {
-//        if (urlsInDomainQ == 0){
-//            nextPopIndex = 0;
-//            return;
-//        }
-//        int ind = EmptyQueuesSet.
-//
-//    }
 
     private int getPriority(String Url) {
         return 1;
@@ -86,27 +61,13 @@ public class QueueManager {
     public boolean moveToDomainQ() {
         Url u = priorityQ.poll();
 
-        //checking for empty priority queue
         if (u == null)
             return false;
 
         String domain = u.getDomainName();
-
-//        Integer index = domainIndexMp.get(domain);
-//
-//        if (index == null) {
-//            index = createDomainQueue(domain);
-//            if (index == -1) {
-//                System.out.println("Cannot move url to domain queue");
-//                u.decrementPriority();              //decreasing priority so that it doesn't get again into the most prior one
-//                priorityQ.add(u);                   //return to priorityQ
-//            }
-//        }
-//        domainQList[index].add(u);
-//        urlsInDomainQ++;
-//        return true;
-
         Queue<Url> q = domainIndexMp.get(domain);
+
+        //handling case of no available space in the "domainQs" list
         if (q == null) {
             q = createDomainQueue(domain);
             if (q == null) {
@@ -121,20 +82,9 @@ public class QueueManager {
     }
 
     //This fn returns:
-    // 1-the index of the created queue if available
-    // 2- (-1) incase of no available queues in the list
+    // 1- reference to the domain queue if there exist available space
+    // 2- null incase of no available queues in the list
     private Queue<Url> createDomainQueue(String domain) {
-//        Integer ind = EmptyQueuesSet.Ceiling((Integer) 0);
-//        if (ind == null) {
-//            if (domainQListSize == domainQCapacity)
-//                return -1;
-//            ind = domainQListSize++;
-//            domainQList[ind] = new LinkedList<>();
-//        } else {
-//            EmptyQueuesSet.remove(ind);
-//        }
-//        domainIndexMp.put(domain, ind);
-//        return ind;
 
         if (domainQListSize == domainQCapacity){
             System.out.println("Cannot create more domain queues");
@@ -170,6 +120,7 @@ public class QueueManager {
     }
 }
 
+//implements fn compare to be able to sort Linkedlists inside the priority queue
 class llComparator implements Comparator<LinkedList<Url>> {
     public int compare(LinkedList<Url> l1, LinkedList<Url> l2) {
         if (l1.size() < l2.size())
