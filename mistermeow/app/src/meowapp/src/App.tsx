364e19d4d311b2be07fd10c333a1b6121cc6894b
amir-kedis <amir.kedis@gmail.com>
2024-05-06 18:20:24 +0300

    feat: change theme with classes

--- mistermeow/app/src/meowapp/src/App.tsx
+++ mistermeow/app/src/meowapp/src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
     <>
-      <div className="rose bg-home flex text-logo flex-col items-center justify-center min-h-screen bg-gray-100">
-        Hello World
+      <div id="theme" className="rose black dark">
+        <div className="bg-home flex text-logo flex-col items-center justify-center min-h-screen bg-gray-100">
+          Hello World
+        </div>
       </div>
     </>
   );
