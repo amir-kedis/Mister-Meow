b24c2dcef44bf3e12214c435d6619d967c640b22
amir-kedis <amir.kedis@gmail.com>
2024-05-06 18:07:46 +0300

    chore: generate color classes in tailwind

--- mistermeow/app/src/meowapp/tailwind.config.js
+++ mistermeow/app/src/meowapp/tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 const generateColorClass = (variable) => {
-  return ({ opacityValue }) =>
-    opacityValue
-      ? `rgba(var(--${variable}), ${opacityValue})`
-      : `rgb(var(--${variable}))`;
+  return ({ opacityValue }) => `var(--${variable})`;
+};
+
+const textColor = {
+  logo: generateColorClass("primary-txt"),
+  logoCaption: generateColorClass("secondary-txt"),
+  nav: generateColorClass("bottom-nav-txt"),
+  btn: generateColorClass("btn-txt"),
+  icon: generateColorClass("icon-color"),
+  caption: generateColorClass("cap-text"),
+  "sr-tag": generateColorClass("tag-primary"),
+  "sr-host": generateColorClass("sr-host"),
+  "sr-url": generateColorClass("sr-url"),
+  "sr-title": generateColorClass("sr-title"),
+  "sr-snippet": generateColorClass("sr-snippet"),
+};
+
+const backgroundColor = {
+  home: generateColorClass("home-bg"),
+  nav: generateColorClass("bottom-nav-bg"),
+  btn: generateColorClass("btn-bg"),
+  "nav-icon": generateColorClass("nav-icon-bg"),
+  tie: generateColorClass("tie-color"),
+  "sr-tag": generateColorClass("tag-secondary"),
 };
 
 module.exports = {
@@ -15,6 +35,11 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
-  theme: {},
+  theme: {
+    extend: {
+      textColor,
+      backgroundColor,
+    },
+  },
   plugins: [require("tailwindcss-animate")],
 };
