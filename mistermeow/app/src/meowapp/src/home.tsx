04b51961b32ee0b8aa2b25a41c65774612bfa881
amir-kedis <amir.kedis@gmail.com>
2024-05-09 20:56:15 +0300

    feat: form btn

--- mistermeow/app/src/meowapp/src/home.tsx
+++ mistermeow/app/src/meowapp/src/home.tsx
@@ -5,14 +5,28 @@ import { ThemeContext } from "./contexts/themeContext.tsx";
 import { useContext } from "react";
 
 function Home() {
-  const { theme, setTheme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Search button clicked");
+  };
+
   return (
     <div
       className={` ${theme} flex flex-col bg-home text-primary h-screen fill-current`}
     >
       <div className="flex-grow flex flex-col justify-center items-center">
         <Banner />
-        <SearchBar />
+        <form onSubmit={submitHandler}>
+          <SearchBar />
+          <button
+            type="submit"
+            className="mt-4 bg-btn text-btn font-inter font-semibold text-base rounded pl-4 pr-4 pt-2 pb-2 hover:filter hover:saturate-50 transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
+          >
+            Meow Search
+          </button>
+        </form>
       </div>
       <Footer />
     </div>
