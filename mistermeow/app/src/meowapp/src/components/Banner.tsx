f72f0e6ff60c30b8415fba4daadd46c511d5b1ff
amir-kedis <amir.kedis@gmail.com>
2024-05-09 23:32:00 +0300

    fix: focus bug on searchbar

--- mistermeow/app/src/meowapp/src/components/Banner.tsx
+++ mistermeow/app/src/meowapp/src/components/Banner.tsx
@@ -1,17 +1,27 @@
 import Logo from "@/assets/icons/logo";
 
-function Banner() {
+export interface BannerProps {
+  size?: string;
+}
+
+function Banner({ size }: BannerProps) {
   return (
     <div>
       <div className="flex justify-center gap-3 place-items-center">
-        <Logo size="lg" />
-        <h1 className="text-5xl font-bilya font-bold tracking-widest max-w-[6ch] leading-normal">
+        <Logo size={size} />
+        <h1
+          className={`text-${
+            size == "sm" ? "base" : "5xl"
+          } font-bilya font-bold tracking-widest max-w-[6ch] leading-normal`}
+        >
           Mister Meow
         </h1>
       </div>
-      <h2 className=" mt-4 text-lg text-center font-bilya font-bold tracking-widest">
-        THE CURiOUS SEARCHER
-      </h2>
+      {size != "sm" && (
+        <h2 className=" mt-4 text-lg text-center font-bilya font-bold tracking-widest">
+          THE CURiOUS SEARCHER
+        </h2>
+      )}
     </div>
   );
 }
