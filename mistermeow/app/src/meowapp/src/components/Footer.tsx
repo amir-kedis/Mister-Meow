cf8a4e83ee4c072461f293c8772e7b0da9eb70c3
amir-kedis <amir.kedis@gmail.com>
2024-05-09 19:48:30 +0300

    feat: footer done

--- mistermeow/app/src/meowapp/src/components/Footer.tsx
+++ mistermeow/app/src/meowapp/src/components/Footer.tsx
@@ -1,17 +1,28 @@
 import ThemeChanger from "./ThemeChanger";
 import VersionIcon from "../assets/icons/VersionIcon.tsx";
 
+import { fetchTags } from "@/utils/external-api.tsx";
+import { useQuery } from "react-query";
+
 function Footer() {
+  const { data, isLoading, isError } = useQuery("tags", fetchTags);
   return (
     <div className="text-nav bg-nav text-base p-3">
       <div className="container text-center flex justify-between align-center">
         <div className="font-bilya">EGYPT</div>
         <div className="font-jetbrains flex gap-4 ">
           <ThemeChanger />
-          <span className="cursor-pointer">
-            <VersionIcon className="inline mr-2" />
-            v0.0.1
-          </span>
+          <a
+            href="https://github.com/amir-kedis/Mister-Meow/"
+            className="cursor-pointer"
+          >
+            <VersionIcon className="inline fill-current mr-2" />
+            {isLoading
+              ? "Loading..."
+              : isError
+              ? "Failed to fetch version"
+              : "v" + data[0]?.name}
+          </a>
         </div>
       </div>
     </div>
--- mistermeow/app/src/meowapp/src/home.tsx
+++ mistermeow/app/src/meowapp/src/home.tsx
@@ -1,10 +1,13 @@
 import SearchBar from "./components/SearchBar.tsx";
 import Footer from "./components/Footer.tsx";
 import Banner from "./components/Banner.tsx";
+import { ThemeContext } from "./contexts/themeContext.tsx";
+import { useContext } from "react";
 
 function Home() {
+  const { theme, setTheme } = useContext(ThemeContext);
   return (
-    <div className="flex flex-col h-screen fill-current">
+    <div className={` ${theme} flex flex-col h-screen fill-current`}>
       <div className="flex-grow flex flex-col justify-center items-center">
         <Banner />
         <SearchBar />
