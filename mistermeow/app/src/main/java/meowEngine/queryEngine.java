8090df34c1fb4672ecd5ac2aaf5ed54aeb813a49
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-05-11 21:19:43 +0300

    feat: startup engine setup

--- mistermeow/app/src/main/java/meowEngine/queryEngine.java
+++ mistermeow/app/src/main/java/meowEngine/queryEngine.java
@@ -1,5 +1,7 @@
 package meowEngine;
 
+import javax.ws.rs.*;
+
 /*
  * get search query from frontend
  * tokenize and do all other necessary processing
@@ -16,14 +18,64 @@ package meowEngine;
  *
  * TODO:
  * 1. create the interface of the API
- * 2. get suggestions -> Endpoint
- * 3. post search query -> Endpoint
- * 4. post pagination -> Endpoint
- * 5. get all results metadata -> Endpoint
- * 6. get all results into -> Endpoint
+ * 2. get suggestions -> Endpoint (GET /suggestions)
+ * 3. post search query -> Endpoint (POST /search)
+ * 4. post pagination -> Endpoint (POST /page)
+ * 5. get all results metadata -> Endpoint (GET /metaResults)
+ * 6. get all results info -> Endpoint (GET /results)
  * 7. preprocess the query -> middleware
  * 8. search in the indexer and get all docs -> middleware
  * 9. rank the docs -> middleware
  * 10. Save all docs in a list -> middleware
  *
  */
+
+@Path("/")
+public class queryEngine {
+
+  @GET
+  @Path("/suggestions")
+  public String getSuggestions() {
+    return "Suggestions";
+  }
+
+  @POST
+  @Path("/search")
+  public String searchQuery() {
+    return "Search Query";
+  }
+
+  @POST
+  @Path("/page")
+  public String changePagination() {
+    return "Pagination";
+  }
+
+  @GET
+  @Path("/metaResults")
+  public String getResultsMetadata() {
+    return "Results Metadata";
+  }
+
+  @GET
+  @Path("/results")
+  public String getResultsInfo() {
+    return "Results Info";
+  }
+
+  public void preprocessQuery() {
+    System.out.println("Preprocess Query");
+  }
+
+  public void searchIndexer() {
+    System.out.println("Search Indexer");
+  }
+
+  public void rankDocs() {
+    System.out.println("Rank Docs");
+  }
+
+  public void saveCurrentDocs() {
+    System.out.println("Save Docs");
+  }
+}
