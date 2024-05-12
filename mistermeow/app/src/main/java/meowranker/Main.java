5c9e085c4bf97d076bb084789d16719c5d19b2e7
AbdelruhmanSamy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-05-12 18:56:20 +0300

    feat: testing construct graph

--- mistermeow/app/src/main/java/meowranker/Main.java
+++ mistermeow/app/src/main/java/meowranker/Main.java
@@ -1,34 +1,28 @@
 package meowranker;
 
 import java.util.*;
-
 import org.bson.types.ObjectId;
 
 public class Main {
     public static void main(String[] argv) {
 
-        // int UrlCount = 4;
-        // double[][] M = new double[UrlCount][];
+        Ranker ranker = new Ranker();
 
-        // M[0] = new double[]{0 , 1 , 0, 0};
-        // M[1] = new double[]{0 , 0 , 0.5, 1};
-        // M[2] = new double[]{1 , 0 , 0, 0};
-        // M[3] = new double[]{0 , 0 , 0.5, 0};
+        double[][] M = ranker.constructUrlsGraph();
+       
+        double[] R = ranker.getPopularity(M , M.length);
 
-        // Ranker ranker = new Ranker();
-        // double[] r = ranker.getPopularity(M , UrlCount);
-
-        // for(int i=0 ; i<UrlCount; i++)
-        // System.out.print(r[i]+ " ");
+        for(int i=0 ; i<M.length; i++)
+            System.out.println(R[i]);
 
         // testing phrase matching
-        PhraseRanker r = new PhraseRanker();
+        // PhraseRanker phRanker = new PhraseRanker();
 
-        String query = "The Free Encyclopedia";
-        r.rank(query); 
+        // String query = "The Free Encyclopedia";
+        // phRanker.rank(query); 
         
-        query = "domestic cat";
-        r.rank(query); 
+        // query = "domestic cat";
+        // phRanker.rank(query); 
         
     }   
 }
