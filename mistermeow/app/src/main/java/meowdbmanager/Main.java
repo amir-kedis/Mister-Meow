1deb92feaea2db759943520beca6a8acebaffd8a
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-05-12 22:38:48 +0300

    feat: get TF in DB manager

--- mistermeow/app/src/main/java/meowdbmanager/Main.java
+++ mistermeow/app/src/main/java/meowdbmanager/Main.java
@@ -1,47 +1,12 @@
 package meowdbmanager;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import meowindexer.Tokenizer;
-import meowindexer.Tokenizer.Token;
-import org.bson.Document;
+import org.bson.types.ObjectId;
 
 public class Main {
   public static void main(String[] args) {
-    // Create a new instance of DBManager
     DBManager dbManager = new DBManager();
-
-    // Test inserting a document
-    String docId = dbManager.insertDocument("http://example.com", "Title", "example.com",
-        "Content", "asdflkasd", "asdfklsdflceww", 1, new ArrayList<Integer>());
-    System.out.println("Inserted document ID: " + docId);
-
-    // Test getting a document
-    Document document = dbManager.getDocument(docId);
-    System.out.println("Retrieved document: ");
-    System.out.println(document.toJson());
-
-    // Test updating the document's indexed status
-    List<Document> docs = dbManager.getDocumentsNotIndexed(5);
-    int i = 1;
-    for (Document doc : docs) {
-      System.out.println("document " + i + ": ");
-      System.out.println(doc.toJson());
-      i++;
-    }
-
-    // Test inserting inverted index
-    HashMap<String, Token> tokens = new HashMap<>();
-    Tokenizer Tokenizer = new Tokenizer();
-    Token token = Tokenizer.new Token("example");
-    token.count = 1;
-    token.position = "h1";
-    tokens.put("example", token);
-    dbManager.insertInverted(docId, tokens);
-
-    // Test getting the inverted index
-    Document invertedIndex = dbManager.getInvertedIndex("example");
-    System.out.println("Retrieved inverted index: " + invertedIndex.toJson());
+    ObjectId id = new ObjectId("6618244771bde05543ad75d8");
+    double tf = dbManager.getDocumentFromInverted("big", id);
+    System.out.println(tf);
   }
 }
