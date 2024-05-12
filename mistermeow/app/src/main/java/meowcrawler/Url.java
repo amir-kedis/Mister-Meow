f0bb475e262137e762c4ac6dd4e610172aa3ff77
akramhany <akramhany65@gmail.com>
2024-05-12 03:00:11 +0300

    feat: Implement ranker graph creation

--- mistermeow/app/src/main/java/meowcrawler/Url.java
+++ mistermeow/app/src/main/java/meowcrawler/Url.java
@@ -10,6 +10,7 @@ public class Url {
   private static final int minPriority = 3;
   private String hashedURL;
   private String hashedDoc;
+  private int rankerId;
 
   public Url(String s, int p) {
     this.urlString = s;
@@ -19,12 +20,33 @@ public class Url {
   }
 
   /**
+   * Setter for the rankerId data member.
+   *
+   * @param id - the value to set with.
+   * @return void.
+   */
+  public void setRankerId(int id) {
+    this.rankerId = id;
+  }
+
+  /**
+   * Getter for the rankerId data member.
+   *
+   * @return id.
+   */
+  public int getRankerId() {
+    return this.rankerId;
+  }
+
+  /**
    * Setter for the hashedURL data member.
    *
    * @param value - the value to set with.
    * @return void.
    */
-  public void setHashedURL(String value) { this.hashedURL = value; }
+  public void setHashedURL(String value) {
+    this.hashedURL = value;
+  }
 
   /**
    * Setter for the hashedDoc data member.
@@ -32,21 +54,27 @@ public class Url {
    * @param value - the value to set with.
    * @return void.
    */
-  public void setHashedDoc(String value) { this.hashedDoc = value; }
+  public void setHashedDoc(String value) {
+    this.hashedDoc = value;
+  }
 
   /**
    * Getter for the hashedURL data member.
    *
    * @return hashedURL.
    */
-  public String getHashedURL() { return this.hashedURL; }
+  public String getHashedURL() {
+    return this.hashedURL;
+  }
 
   /**
    * Getter for the hashedDoc data member.
    *
    * @return hashedDoc.
    */
-  public String getHashedDoc() { return this.hashedDoc; }
+  public String getHashedDoc() {
+    return this.hashedDoc;
+  }
 
   /**
    * Fetches the htmlDoc of the URL with the urlString datamember and returns
@@ -70,7 +98,9 @@ public class Url {
     return "{ " + this.urlString + ", " + this.priority + " }";
   }
 
-  public int getPriority() { return priority; }
+  public int getPriority() {
+    return priority;
+  }
 
   public boolean decrementPriority() {
     if (priority > minPriority) {
@@ -80,7 +110,9 @@ public class Url {
     return false;
   }
 
-  public String getUrlString() { return urlString; }
+  public String getUrlString() {
+    return urlString;
+  }
 
   public String getDomainName() {
     return this.urlString.split("/")[2];
