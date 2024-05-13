459c3f3dbdd7c87ad5188fdf583a5b896588ad48
AbdelruhmanSamy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-05-14 02:33:17 +0300

    feat: QueryRanker is done

---
+++ mistermeow/app/src/main/java/meowranker/QueryRanker.java
@@ -0,0 +1,56 @@
+package meowranker;
+
+import java.util.*;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.jsoup.Jsoup;
+
+public class QueryRanker extends Ranker{
+    
+    public List<Double> QueryDocRel;
+
+    public QueryRanker(){
+        super();
+        QueryDocRel = new ArrayList<>();
+    }
+
+    protected List<ObjectId> getMatchingDocs(List<String> searchTokens, String query){
+        List<ObjectId> docsUnion = db.getDocIDs(searchTokens);
+
+        Map<ObjectId , Integer> docRepetition = new HashMap<>();
+
+        List<ObjectId> distinctDocs = new ArrayList<>();
+
+        ProcessedDocs = new ArrayList<>();
+        for(ObjectId docId : docsUnion){
+            if(docRepetition.containsKey(docId)){
+                // System.out.println(docId);
+                int currRep = docRepetition.get(docId);
+                docRepetition.remove(docId);
+                docRepetition.put(docId , currRep+1);
+            }
+            else{
+                distinctDocs.add(docId);
+                docRepetition.put(docId ,1 ); 
+                ProcessedDocs.add(db.getDocument(docId.toString()));
+            }
+        }
+        
+        for(ObjectId docId :distinctDocs){
+            QueryDocRel.add((double)docRepetition.get(docId)/(double)searchTokens.size());
+        }
+
+        return distinctDocs;
+    }    
+
+    protected List<Double> addQueryDocRel(List<Double> relevance){
+        int ind = 0;
+        for(Double val:relevance){
+            val*=QueryDocRel.get(ind);
+            ind++;
+        }
+
+
+        return relevance;   
+    }
+}
