e70f7a375889767116a62cc2101f3d6e4e69b554
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-05-13 15:39:16 +0300

    fix: connect ranker with engine

--- mistermeow/app/src/main/java/meowranker/Main.java
+++ mistermeow/app/src/main/java/meowranker/Main.java
@@ -5,17 +5,17 @@ import org.bson.types.ObjectId;
 
 public class Main {
     public static void main(String[] argv) {
-        
+
         PhraseRanker phRanker = new PhraseRanker();
 
-        String query = "The dfl;akjf;asd Free Encyclopedia";    //  tests searching for unfound token
-        phRanker.rank(query); 
-        
-        query = "The Free Encyclopedia";                        
-        phRanker.rank(query); 
-        
-        query = "domestic cat";
-        phRanker.rank(query); 
-        
-    }   
+        String query = "The dfl;akjf;asd Free Encyclopedia"; // tests searching for unfound token
+        phRanker.rank(query);
+
+        query = "The Free Encyclopedia";
+        phRanker.rank(query);
+
+        query = "I love you";
+        phRanker.rank(query);
+
+    }
 }
