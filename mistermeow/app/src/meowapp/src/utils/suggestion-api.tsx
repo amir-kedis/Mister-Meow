4f0111d8f8d097c64cda88fa71256959c2843c40
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-05-13 16:24:52 +0300

    feat: suggestion api is now correct

--- mistermeow/app/src/meowapp/src/utils/suggestion-api.tsx
+++ mistermeow/app/src/meowapp/src/utils/suggestion-api.tsx
@@ -1,27 +1,32 @@
 async function fetchSuggestions(query: string) {
-  return Promise.resolve([
-    "meow",
-    "Cats are the best",
-    "Cats Loves Mister Meow",
-    "Mister Meow is the best",
-    "Cats are better than you so you are not the best",
-    "Cats: the history search will be silly",
-  ]);
-  const response = await fetch(`mister-meow.com/suggestions?q=${query}`);
-  if (!response.ok) {
-    // throw new Error("Failed to fetch suggestions");
-    return {
-      suggestions: [
-        "meow",
-        "Cats are the best",
-        "Cats Loves Mister Meow",
-        "Mister Meow is the best",
-        "Cats are better than you so you are not the best",
-        "Cats: the history search will be silly",
-      ],
-    };
-  }
-  const suggestions = await response.json();
+  // return Promise.resolve([
+  //   "meow",
+  //   "Cats are the best",
+  //   "Cats Loves Mister Meow",
+  //   "Mister Meow is the best",
+  //   "Cats are better than you so you are not the best",
+  //   "Cats: the history search will be silly",
+  // ]);
+  const response = await fetch(
+    `http://localhost:8080/suggestions?query=${query}`,
+  );
+  // if (!response.ok) {
+  //   // throw new Error("Failed to fetch suggestions");
+  //   return {
+  //     suggestions: [
+  //       "meow",
+  //       "Cats are the best",
+  //       "Cats Loves Mister Meow",
+  //       "Mister Meow is the best",
+  //       "Cats are better than you so you are not the best",
+  //       "Cats: the history search will be silly",
+  //     ],
+  //   };
+  // }
+
+  let suggestions = await response.json();
+  suggestions = suggestions.data;
+  console.log(suggestions);
   return suggestions;
 }
 
