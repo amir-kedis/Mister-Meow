df72e5787205fbf40445bf44eed098093e80f62a
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-05-14 07:31:49 +0300

    fix: some bugs related to connection between ranker and engine

--- mistermeow/app/src/main/java/meowranker/Main.java
+++ mistermeow/app/src/main/java/meowranker/Main.java
@@ -1,28 +1,25 @@
 package meowranker;
 
-import java.util.*;
-import org.bson.types.ObjectId;
-
 public class Main {
     public static void main(String[] argv) {
 
-        Ranker ranker = new PhraseRanker();
-
-        String query = "The dfl;akjf;asd Free Encyclopedia"; // tests searching for unfound token
-        ranker.rank(query);
-
-        query = "The Free Encyclopedia";
-        ranker.rank(query);
-
-        // query = "Wikipedia";
+        // Ranker ranker = new PhraseRanker();
+        //
+        // String query = "The dfl;akjf;asd Free Encyclopedia"; // tests searching for
+        // unfound token
+        // ranker.rank(query);
+        //
+        // query = "The Free Encyclopedia";
+        // ranker.rank(query);
+        //
+        // // query = "Wikipedia";
+        // // ranker.rank(query);
+        //
+        // query = "I love you";
         // ranker.rank(query);
 
-        query = "I love you";
-        ranker.rank(query);
-
-
-        ranker = new QueryRanker();
-        ranker.rank("The Free Encyclopedia");
+        Ranker ranker = new QueryRanker();
+        ranker.rank("cats");
 
         // QueryRanker Qr = new QueryRanker();
         // Qr.rank("Wkiipedia the free encyclopedia");
